984873dc056d26d9aabe9aa88c616d61e79a3f27
Bett <bettthetimetraveller@gmail.com>
2019-08-04 21:05:03 +0300

    first commit

---
+++ articles/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Article
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article':article})
